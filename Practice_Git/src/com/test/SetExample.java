8a6f16d8797f6a654d562e6b753dd753f77406d0
sandiprodge90@gamil.com <sandiprodge@gmail.com>
2022-09-14 22:21:00 +0530

    set updated

---
+++ Practice_Git/src/com/test/SetExample.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		Set s=new HashSet();
+		
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+		
+	}
+}
