bd8a5bf7170f8fb6ef84558b057cb5697f9b5b16
sandiprodge90@gamil.com <sandiprodge@gmail.com>
2022-09-14 21:55:56 +0530

    New change updated

---
+++ Practice_Git/src/com/test/ArraylistDemo.java
@@ -0,0 +1,19 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraylistDemo {
+	public static void main(String[] args) {
+		
+		List list =new ArrayList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		System.out.println(list);
+	
+	}
+
+}
