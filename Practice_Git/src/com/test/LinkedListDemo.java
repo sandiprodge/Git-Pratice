c8b12aa46f2df5b70508df1ec420d78370a8d21a
sandiprodge90@gamil.com <sandiprodge@gmail.com>
2022-09-14 22:14:39 +0530

    linkedList updated

---
+++ Practice_Git/src/com/test/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	List l=new LinkedList();
+	l.add(45);
+	l.add(74);
+	l.add(50);
+	
+	System.out.println(l);
+}
+}
